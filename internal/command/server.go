f241d479ce880848ee79583ce6f3a58f640e5ebf
Behzad Ehsani <ehsani.eof@gmail.com>
2023-10-06 12:58:24 +0330

    chore: change command name

--- internal/command/server.go
+++ internal/command/server.go
@@ -2,17 +2,17 @@ package command
 
 import "github.com/behzade/go-template/internal/controller"
 
-type ServeCommand struct{}
+type ServerCommand struct{}
 
-func (ServeCommand) Run() (string, error) {
+func (ServerCommand) Run() (string, error) {
 	err := controller.New().Run()
 	return "", err
 }
 
-func (ServeCommand) Name() string {
-	return "serve"
+func (ServerCommand) Name() string {
+	return "server"
 }
 
-func (ServeCommand) Description() string {
+func (ServerCommand) Description() string {
 	return "Run the router and serve http requests"
 }
