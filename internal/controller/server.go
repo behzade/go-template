098bb2c72113020f5ab54efbe9fa2444927c352b
Behzad Ehsani <ehsani.eof@gmail.com>
2023-10-06 19:20:53 +0330

    feat: add openapi spec endpoint

--- internal/controller/server.go
+++ internal/controller/server.go
@@ -22,6 +22,16 @@ func New() *echo.Echo {
 	server := NewStrictHandler(&Server{}, nil)
 	router := echo.New()
 
+	router.GET("openapi.json",func(c echo.Context) error {
+		swagger, err := GetSwagger()
+		swagger.Servers = nil
+		if err != nil {
+			return err
+		}
+
+		return c.JSON(200, swagger)
+	})
+
 	RegisterHandlers(router, server)
 
 	return router
